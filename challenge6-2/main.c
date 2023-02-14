586c44ac368109d1bf87324392b91f01e665df08
Mohammed Zameel ali <55248408+Zameel-Byte@users.noreply.github.com>
2023-02-14 19:35:21 +0530

    Create main.c

---
+++ challenge6-2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "addition.h"
+#include "subtraction.h"
+#include "multiplication.h"
+#include "division.h"
+
+int main() {
+    double a, b;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);
+    printf("%lf + %lf = %lf\n", a, b, add(a, b));
+    printf("%lf - %lf = %lf\n", a, b, subtract(a, b));
+    printf("%lf * %lf = %lf\n", a, b, multiply(a, b));
+    printf("%lf / %lf = %lf\n", a, b, divide(a, b));
+    return 0;
+}
